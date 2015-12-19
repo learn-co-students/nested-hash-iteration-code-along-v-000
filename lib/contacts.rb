7471c9f3d17178bbadae8adca9cdfb151f80c85e
kylegraydev <kylegraydev@gmail.com>
2015-12-18 21:59:51 -0800

    finished

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,5 +14,28 @@ require 'pry'
   #   }
   # }
 def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
 
+    contact_details_hash.each do |attribute, data|
+
+      if attribute == :favorite_icecream_flavors
+
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
+contacts =
+{
+    "Jon Snow" => {
+        name: "Jon",
+        email: "jon_snow@thewall.we",
+        favorite_icecream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+        name: "Freddy",
+        email: "freddy@mercury.com",
+        favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+}
+remove_strawberry(contacts)
