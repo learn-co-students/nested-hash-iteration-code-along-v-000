bf06b3d5f91132f01afa3a3ad0a61c6e66995ef0
Chris Bruen <bruen.chris@gmail.com>
2015-12-27 13:34:35 -0500

    created method

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+	contacts.each do |x, y|
+		y.each do |attribute, data|
+			if attribute == :favorite_icecream_flavors
+				data.delete_if {|ice_cream| ice_cream == "strawberry"}
+			end
+		end
+	end
 
 end
