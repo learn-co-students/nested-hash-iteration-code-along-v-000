c78692c5e746ae7106fcd8958c9166e96791ead8
johnsontaiwo <josseytee@gmail.com>
2016-02-25 10:47:55 -0500

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,7 +14,8 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
+
+ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
     if attribute == :favorite_icecream_flavors
@@ -23,3 +24,4 @@ def remove_strawberry(contacts)
   end
  end
 end
+  
