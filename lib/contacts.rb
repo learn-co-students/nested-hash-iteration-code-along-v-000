dae160517b9c7617dab1506d1d9f52139d18eac7
whalien52 <kinseybasinger@gmail.com>
2016-03-21 15:08:34 -0500

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
+contacts.each do |person, data_hash|
+  data_hash.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if {|ice_cream| ice_cream == "strawberry"}
+    end
+  end
+end
 
 end
