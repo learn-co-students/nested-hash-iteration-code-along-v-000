44e3eceb3e82b6cb77326c9214f1640df0269e77
Jesse Novotny <jessenovotny@gmail.com>
2016-06-16 16:48:52 -0700

    Done.

--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -20,6 +20,6 @@ describe "#remove_strawberry" do
 
   it "removes the strawberry ice cream from :favorite_ice_cream_flavors" do
     result = remove_strawberry(contacts)
-    expect(contacts["Freddy Mercury"][:favorite_ice_cream_flavors]).to_not include("strawberry")
+    expect(result["Freddy Mercury"][:favorite_ice_cream_flavors]).to_not include("strawberry")
   end
 end
