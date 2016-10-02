5c99eaf8960269c6553d9ca697c674d0e43fef85
Marine Boudeau <m14868@gmail.com>
2016-10-02 22:14:09 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, data|
+    data.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if do |ice_cream|
+          ice_cream == "strawberry"
+        end
+      end
+    end
+  end
 end
