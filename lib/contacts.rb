82615f40d1406dc4769a890c0ea83b71989cb439
gb23 <gregbenj@hotmail.com>
2017-05-21 18:53:32 -0700

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+    contacts.each {|person, contact_details_hash| 
+      contact_details_hash.each{|attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if{ |ice_cream| ice_cream == "strawberry" }
+        end
+      }
+    }
 end
