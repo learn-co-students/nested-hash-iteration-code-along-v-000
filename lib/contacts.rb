6e6003cf21acffc11fae506be28cd9af394a9d8d
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-26 18:47:43 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,10 +1,22 @@
 require 'pry'
 
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +25,20 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
-def remove_strawberry(contacts)
 
+def remove_strawberry2(contacts)
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
+
+def remove_strawberry(contacts)
+  contacts["Freddy Mercury"][:favorite_ice_cream_flavors].delete_if{|flav| flav == "strawberry"}
+  puts contacts["Freddy Mercury"][:favorite_ice_cream_flavors].inspect
+end
+
+remove_strawberry(contacts)
