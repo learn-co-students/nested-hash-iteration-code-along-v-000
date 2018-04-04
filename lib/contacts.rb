796128adfe690bb24652c4aefdd315f5e499853b
Chris Dundon <chris.dundon@gmail.com>
2018-04-04 02:01:28 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,18 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |k, v|
+    rtn = []
+    v.each do |k_, v_|
+      if k_ == :favorite_ice_cream_flavors
+        v_.each do |e|
+          if e != "strawberry"
+            rtn << e
+          end
+        end
+      end
+    end
+    v[:favorite_ice_cream_flavors] = rtn
+  end
+  contacts
 end
