2ed4a5afe964a8c0e3e1244894dcba6137c729a2
Alex Zhang <alexgzhang92@gmail.com>
2018-05-28 01:11:38 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |name, items|
+    items.each do |key, value|
+      if key == :favorite_ice_cream_flavors
+        value.delete_if { |flavor| flavor == "strawberry"}
+      end
+    end
+  end
 end
+
