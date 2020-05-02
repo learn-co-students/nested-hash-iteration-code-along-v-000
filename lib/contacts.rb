9f1f255214be32f720759d47b4f8dafe5b4ffa56
idith almog <almog.idith@gmail.com>
2020-05-02 01:31:15 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -13,7 +13,29 @@ require 'pry'
   #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
-  
+  contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we",
+    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
 def remove_strawberry(contacts)
+  contacts.each do |person,data|
+    if person ==   "Freddy Mercury"
 
+      data.each do |attribute,value|
+        if attribute == :favorite_ice_cream_flavors
+
+          value.shift
+        end
+      end
+    end
+  end
+contacts
 end
