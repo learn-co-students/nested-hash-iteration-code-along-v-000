0f80553c6ada9e96cf148622ec62a6e309f9a752
lrh2daizzo <herrera.lrh@gmail.com>
2023-02-28 19:24:19 -0600

    reset

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,12 +15,6 @@ require 'pry'
   # }
 
 def remove_strawberry(contacts)
-  contacts.each {|person, contact_details_hash|
-    contact_details_hash.each {|attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
-      end
-    }
-  }
+
 
 end
